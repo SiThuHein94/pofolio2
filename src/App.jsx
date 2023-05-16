0525c5c9cb1e1b20313f86d8ca9ab682554110dd
Si Thu Hein <mgcthu94@gmail.com>
2023-05-17 00:19:09 +0630

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ParticlesBackground from "./tools/particles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const App = () => {
+
+
+  return (
+    <>
+   
+      <ParticlesBackground />
+      <div className="bg-black" > Si Thu Hein </div>
+    </>
+
+  )
+}
+export default App
---
+++ src/tools/particles.jsx
@@ -0,0 +1,86 @@
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+const ParticlesBackground = () => {
+    const particlesInit = useCallback(async engine => {
+        await loadFull(engine);
+    }, []);
+    return (
+        <Particles
+
+            id="tsparticles"
+            init={particlesInit}
+            // loaded={particlesLoaded}
+            options={{
+
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 50,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#000",
+                    },
+                    links: {
+                        color: "#000",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.1,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce-vertical",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 500,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.1,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        ></Particles>
+    );
+};
+export default ParticlesBackground
